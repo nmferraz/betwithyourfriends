19b0ca361edbb708cc8a2d1022275b6359bc95bc
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-14 13:49:52 +0000

    installing nativebase

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>BetWithYourFriends</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>BetWithYourFriends</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-  }
+  },
 });
