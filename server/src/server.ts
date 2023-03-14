5862ff364bd260f6ccb0680c5e97787158729d8a
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-14 10:53:57 +0000

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({ log: ["query"] });
@@ -6,12 +7,16 @@ const prisma = new PrismaClient({ log: ["query"] });
 async function bootstrap() {
   const fastify = Fastify({ logger: true });
 
-  fastify.get("/polls/count", async() => {
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  fastify.get("/polls/count", async () => {
     const count = await prisma.poll.count();
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
