6f6b26781cf33e5ac3eb1cb8a5fbf9902d526470
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-15 12:30:27 +0000

    basic html structure

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,14 +4,55 @@ interface HomeProps {
 
 import Image from "next/image";
 import appPreviewImg from "../assets/preview.png";
+import logoImg from "../assets/logo_yellow.svg";
+import usersAvatarExampleImg from "../assets/avatares.png";
+import iconCheckImg from "../assets/icon-check.svg";
 
 export default function Home(props: HomeProps) {
   return (
     <div>
-      <main></main>
+      <main>
+        <Image src={logoImg} alt="BetWithYourFriends Logo" />
+        <h1>Create your own football poll and share it with your friends!</h1>
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" quality={100} />
+          <strong>
+            <span>+2000</span>are already using
+          </strong>
+        </div>
+        <form>
+          <input
+            type="text"
+            required
+            placeholder="Whats the name of your poll game?"
+          />
+          <button type="submit">Create my poll</button>
+        </form>
+        <p>
+          After creating your poll, you will receive a unique code that you can
+          use to invite other people 🚀
+        </p>
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+600</span>
+              <p>Polls created</p>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+600</span>
+              <p>predictions sent</p>
+            </div>
+          </div>
+        </div>
+      </main>
       <Image
         src={appPreviewImg}
         alt="Two phones showing a preview of the mobile app"
+        quality={100}
       />
     </div>
   );
