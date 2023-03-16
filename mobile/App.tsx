6c87beda734c0d15ca65ed15ba140e52ec387a53
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-16 19:52:42 +0000

    using Context and Provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,6 +8,7 @@ import {
 import { Loading } from "./src/components/Loading";
 import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,12 +19,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -2,8 +2,13 @@ import { Center, Icon, Text } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
 import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
+  const { signIn, user } = useAuth();
+
+  console.log(user);
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -11,6 +16,7 @@ export function SignIn() {
         title="ENTER WITH GOOGLE"
         type="SECONDARY"
         mt={12}
+        onPress={signIn}
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
       />
       <Text color="white" textAlign="center" mt={4}>
