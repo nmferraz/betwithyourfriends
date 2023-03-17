e01f59c12bb80dedd66c759e1e1ea3694daa516b
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-17 16:31:45 +0000

    poll routes

--- server/src/routes/poll.ts
+++ server/src/routes/poll.ts
@@ -1,4 +1,6 @@
 import { FastifyInstance } from "fastify";
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export function pollRoutes(fastify: FastifyInstance) {
@@ -6,4 +8,25 @@ export function pollRoutes(fastify: FastifyInstance) {
     const count = await prisma.poll.count();
     return { count };
   });
+    
+      fastify.post("/polls", async (request, reply) => {
+        const createPollBody = z.object({
+          title: z.string(),
+        });
+
+        const { title } = createPollBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+
+        const code = String(generate()).toUpperCase();
+
+        await prisma.poll.create({
+          data: {
+            title,
+            code,
+          },
+        });
+
+        return reply.status(201).send({ code });
+      });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { z } from "zod";
 import ShortUniqueId from "short-unique-id";
+import { pollRoutes } from "./routes/poll";
 
 async function bootstrap() {
   const fastify = Fastify({ logger: true });
@@ -10,28 +11,11 @@ async function bootstrap() {
     origin: true,
   });
 
+  fastify.register(pollRoutes);
+
   
 
-  fastify.post("/polls", async (request, reply) => {
-    const createPollBody = z.object({
-      title: z.string(),
-    });
 
-    const { title } = createPollBody.parse(request.body);
-
-    const generate = new ShortUniqueId({ length: 6 });
-
-    const code = String(generate()).toUpperCase();
-
-    await prisma.poll.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    return reply.status(201).send({ code });
-  });
 
   fastify.get("/users/count", async () => {
     const count = await prisma.user.count();
