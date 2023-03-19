518c6d85e5461a1cded8ff664f117ade5d8f5db8
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-19 02:31:48 +0000

    using navigation in polls screen

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,9 +1,12 @@
 import { VStack, Icon } from "native-base";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 import { Octicons } from "@expo/vector-icons";
 
 export function Polls() {
+  const { navigate } = useNavigation();
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="My Polls" />
@@ -20,6 +23,7 @@ export function Polls() {
           leftIcon={
             <Icon as={Octicons} name="search" color="black" size="md" />
           }
+          onPress={() => navigate("find")}
         />
       </VStack>
     </VStack>
