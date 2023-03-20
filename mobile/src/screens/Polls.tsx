eca5d50e64e3e65a64cffad972d81bdcdb98fb3a
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-20 01:29:21 +0000

    Polls Page

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,11 +1,43 @@
-import { VStack, Icon } from "native-base";
+import { useCallback, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { Octicons } from "@expo/vector-icons";
+import { api } from "../services/api";
+import { PollCard, PollCardPros } from "../components/PollCard";
+import { Loading } from "../components/Loading";
+import { EmptyPollList } from "../components/EmptyPollList";
 
 export function Polls() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [polls, setPolls] = useState<PollCardPros[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPolls() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/polls");
+      setPolls(response.data.polls);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Error loading polls",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPolls();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -26,6 +58,25 @@ export function Polls() {
           onPress={() => navigate("find")}
         />
       </VStack>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={polls}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <PollCard
+              data={item}
+              onPress={() => navigate("details", { id: item.id })}
+            />
+          )}
+          ListEmptyComponent={<EmptyPollList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          px={5}
+        />
+      )}
     </VStack>
   );
 }
